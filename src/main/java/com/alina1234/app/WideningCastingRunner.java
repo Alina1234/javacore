5f234b36b055e7bdf8da08bf4da02aba36621e4e
Alina Tomilenko <agr@ciklum.com>
2017-02-02 10:35:30 +0200

    New class and running (casting)

---
+++ src/main/java/com/alina1234/app/WideningCastingRunner.java
@@ -0,0 +1,12 @@
+package com.alina1234.app;
+
+/**
+ * Created by agr on 2/2/2017.
+ */
+public class WideningCastingRunner {
+
+    public static void main(String[] agrs){
+        WideningCasting casting1 = new WideningCasting();
+        casting1.widening();
+    }
+}
